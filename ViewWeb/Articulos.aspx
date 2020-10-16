765fd163ddcd351d140459ecf78abd35e222a686
rodriguezleandro98 <leandrohodriguez98@gmail.com>
2020-10-16 13:21:09 -0300

    Loggin ok(falta validar mail) y vista art sin nada

---
+++ ViewWeb/Articulos.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Articulos.aspx.cs" Inherits="ViewWeb.Articulos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="Body" runat="server">
+    <h3>Articulos</h3>
+    <asp:Label Style="margin-left:15px" class="col-form-label"  ID="lblBienvenida" Text="Bienvenido: " Visible="false" runat="server" />
+</asp:Content>
---
+++ ViewWeb/Articulos.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ViewWeb
+{
+    public partial class Articulos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session[Session.SessionID + "nombreusuario"] != null)
+                {
+                    lblBienvenida.Visible = true;
+                    lblBienvenida.Text += Session[Session.SessionID + "nombreusuario"].ToString();
+                }
+
+            }
+        }
+    }
+}
